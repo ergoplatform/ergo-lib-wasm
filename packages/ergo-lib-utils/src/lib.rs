c2f2dd334119994835bba4e6f357bcaa4ed3707b
ross-weir <29697678+ross-weir@users.noreply.github.com>
2023-05-18 19:20:21 +1000

    add console panic hook & npm provance

--- packages/ergo-lib-utils/src/lib.rs
+++ packages/ergo-lib-utils/src/lib.rs
@@ -37,3 +37,17 @@ where
         self.map_err(|e| JsValue::from_str(format!("js error occurred: {:?}", e).as_str()))
     }
 }
+
+#[macro_export]
+macro_rules! impl_set_console_panic_hook_fn {
+    () => {
+        /// Set a panic hook that provides richer debug/stack trace information
+        /// on errors that occur in WASM code.
+        ///
+        /// Call this function in the entrypoint of your application.
+        #[wasm_bindgen::prelude::wasm_bindgen(js_name = "__setConsolePanicHook")]
+        pub fn set_console_panic_hook() {
+            console_error_panic_hook::set_once()
+        }
+    };
+}
